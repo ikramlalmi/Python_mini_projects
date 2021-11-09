bcd804a24eed21f2b95d448d0b8fecdcb82fc164
Ikram <ikramlalmi97@gmail.com>
2021-11-09 13:54:27 -0500

    Done!

---
+++ Tic_tac_toe/pla.py
@@ -0,0 +1,95 @@
+import math 
+import random
+
+# from ga import play
+
+class Player():
+    def __init__(self, letter):
+        # either going to O or x
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        # pass in a random valid spot for the next move using the available move fucntion
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        # to store the value input from the user that is actually valid
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\'s turn. to play. Imput move (0-8): " )
+            # check if imput is valid by casting it to an integer,and
+            try:
+                val = int(square)
+                if val not in  game.available_moves():
+                    raise ValueError
+                valid_square = True # the square in valid
+            except ValueError:
+                print("Invalid square. try again!")
+        return val
+
+
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else :
+            square = self.minimax(game, self.letter)['position']
+        return square
+    
+
+    def minimax(self, state, player):
+            max_player = self.letter
+            other_player = 'O' if player == 'X' else 'X'
+
+            # check if the previous move is a winning move. 
+            if state.current_winner == other_player:
+                return {'position' : None , 'score' : 1 * (state.num_empty_square() +1) if other_player == max_player else -1*(state.num_empty_square()+1)}
+            elif not state.empty_square(): 
+                return {"position" : None , 'score' : 0}
+
+            if player == max_player:
+                best = {"position" : None , 'score' : -math.inf} #score max
+            else :
+                best = {"position" : None , 'score' : math.inf} #score min
+
+            for possible_move in state.available_moves():
+                state.make_move(possible_move, player)
+                sim_score = self.minimax(state, other_player)
+
+                # undo the move
+
+                state.board[possible_move] = " "
+                state.current_winner = None
+                sim_score['position'] = possible_move
+
+                if player == max_player:
+                    if sim_score["score"]>best["score"]:
+                        best = sim_score
+
+                else: 
+                    if sim_score['score'] < best['score']:
+                        best = sim_score
+            return best
+
+
+
+
+
+
+
